bbcdda677268b57ea32a2e0285548b7e3deca428
Kay Levin <ks.kwanruan@gmail.com>
2023-12-18 12:51:35 +1100

    WIP commit

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,8 +1,11 @@
 ### Required Libraries ###
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
 import random
 import requests
 import os
-
 
 ### Dialog Actions Helper Functions ###
 def get_slots(intent_request):
@@ -37,6 +40,57 @@ def close(session_attributes, fulfillment_state, message):
     }
 
     return response
+
+### Business Login Function ###
+def calculate_portfolio_metrics(stock_ticker, short_window=20, long_window=100, initial_capital=100000, share_size=500):
+    # Data Preparation
+    getStockData = yf.Ticker(stock_ticker)
+    df = pd.DataFrame(data=getStockData.history(period="max"))
+    signals_df = df.copy()
+
+    # Generate moving averages
+    signals_df['SMA50'] = signals_df['Close'].rolling(window=short_window).mean()
+    signals_df['SMA100'] = signals_df['Close'].rolling(window=long_window).mean()
+
+    # Generate trading signals
+    signals_df['Signal'] = 0.0
+    signals_df['Signal'][short_window:] = np.where(
+        signals_df['SMA50'][short_window:] > signals_df['SMA100'][short_window:], 1.0, 0.0
+    )
+
+    # Entry/Exit signals
+    signals_df['Entry/Exit'] = signals_df['Signal'].diff()
+
+    # Set initial capital
+    signals_df['Position'] = share_size * signals_df['Signal']
+    signals_df['Entry/Exit Position'] = signals_df['Position'].diff()
+    signals_df['Portfolio Holdings'] = signals_df['Close'] * signals_df['Position']
+    signals_df['Portfolio Cash'] = initial_capital - (signals_df['Close'] * signals_df['Entry/Exit Position']).cumsum()
+    signals_df['Portfolio Total'] = signals_df['Portfolio Cash'] + signals_df['Portfolio Holdings']
+    signals_df['Portfolio Daily Returns'] = signals_df['Portfolio Total'].pct_change()
+    signals_df['Portfolio Cumulative Returns'] = (1 + signals_df['Portfolio Daily Returns']).cumprod() - 1
+
+    # Portfolio-Level Risk/Reward Evaluation Metrics
+    portfolio_evaluation_df = pd.DataFrame(index=["Annualized Return", "Cumulative Returns", "Annual Volatility", "Sharpe Ratio", "Sortino Ratio"],
+                                           columns=["Backtest"])
+
+    portfolio_evaluation_df.loc["Annualized Return"] = signals_df["Portfolio Daily Returns"].mean() * 252
+    portfolio_evaluation_df.loc["Cumulative Returns"] = signals_df["Portfolio Cumulative Returns"][-1]
+    portfolio_evaluation_df.loc["Annual Volatility"] = signals_df["Portfolio Daily Returns"].std() * np.sqrt(252)
+    portfolio_evaluation_df.loc["Sharpe Ratio"] = (signals_df["Portfolio Daily Returns"].mean() * 252) / (signals_df["Portfolio Daily Returns"].std() * np.sqrt(252))
+
+    # Sortino Ratio Calculation
+    sortino_ratio_df = signals_df[["Portfolio Daily Returns"]].copy()
+    sortino_ratio_df.loc[:, "Downside Returns"] = 0
+    sortino_ratio_df.loc[sortino_ratio_df["Portfolio Daily Returns"] < 0, "Downside Returns"] = sortino_ratio_df["Portfolio Daily Returns"] ** 2
+
+    annualized_return = sortino_ratio_df["Portfolio Daily Returns"].mean() * 252
+    downside_standard_deviation = np.sqrt(sortino_ratio_df["Downside Returns"].mean()) * np.sqrt(252)
+    sortino_ratio = annualized_return / downside_standard_deviation
+
+    portfolio_evaluation_df.loc["Sortino Ratio"] = sortino_ratio
+
+    return signals_df, portfolio_evaluation_df
 
 ### Intents Handlers ###
 
@@ -99,18 +153,58 @@ def get_market_trends(intent_request):
     )
 
 def get_stock_performance(intent_request):
-     # Gets slots' values
-    stock_name = get_slots(intent_request)["stock_name"]
-    
-    # Return a message with the result (for now, a simple message is returned)
+    # Gets slots' values
+    stock_name = get_slots(intent_request)["stock_ticker"]
+    short_window = get_slots(intent_request)["short_window"] 
+    long_window = get_slots(intent_request)["long_window"] 
+    initial_capital = get_slots(intent_request)["initial_capital"] 
+    share_size = get_slots(intent_request)["share_size"]
+
+    # Retrieve stock data and calculate portfolio metrics
+    try:
+        stock_data, portfolio_metrics = calculate_portfolio_metrics(stock_name, short_window, long_window, initial_capital, share_size)
+    except Exception as e:
+        # Handle any exceptions (e.g., invalid stock symbol)
+        return close(
+            intent_request["sessionAttributes"],
+            "Failed",
+            {
+                "contentType": "PlainText",
+                "content": f"Error: {str(e)}",
+            },
+        )
+
+    # Format the result message
+    result_message = f"Stock performance metrics for {stock_name}:\n"
+    result_message += f"Annualized Return: {portfolio_metrics.loc['Annualized Return']:.2%}\n"
+    result_message += f"Cumulative Returns: {portfolio_metrics.loc['Cumulative Returns']:.2%}\n"
+    result_message += f"Annual Volatility: {portfolio_metrics.loc['Annual Volatility']:.2%}\n"
+    result_message += f"Sharpe Ratio: {portfolio_metrics.loc['Sharpe Ratio']:.2f}\n"
+    result_message += f"Sortino Ratio: {portfolio_metrics.loc['Sortino Ratio']:.2f}"
+
+    # Return the formatted result
     return close(
         intent_request["sessionAttributes"],
         "Fulfilled",
         {
             "contentType": "PlainText",
-            "content": "get_stock_performance! (Stock Performance Logic to be implemented)",
+            "content": result_message,
         },
     )
+
+# def get_stock_performance(intent_request):
+#      # Gets slots' values
+#     stock_name = get_slots(intent_request)["stock_name"]
+    
+#     # Return a message with the result (for now, a simple message is returned)
+#     return close(
+#         intent_request["sessionAttributes"],
+#         "Fulfilled",
+#         {
+#             "contentType": "PlainText",
+#             "content": "get_stock_performance! (Stock Performance Logic to be implemented)",
+#         },
+#     )
     
 def get_news_updates(intent_request):
     # Gets slots' values
